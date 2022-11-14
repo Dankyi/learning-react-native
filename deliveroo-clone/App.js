a17d4b67d214d4e35e3a5ebc809d46de0dfee14d
Dankyi <dankyiboateng15@gmail.com>
2022-11-14 23:07:54 +0000

    deliveroo-clone: configured react stack navigator and created homescreen stack

--- deliveroo-clone/App.js
+++ deliveroo-clone/App.js
@@ -1,12 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Text, View } from "react-native";
+
+import HomeScreen from "./screens/HomeScreen";
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
 	return (
-		<View>
-			<Text className="text-red-400">Open up App.js to start working on your app!</Text>
-			<StatusBar style="auto" />
-		</View>
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen name="home" component={HomeScreen} />
+			</Stack.Navigator>
+		</NavigationContainer>
 	);
 }
 
