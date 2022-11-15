80b4d75051b704a8395ac3820d2a901dec850267
Dankyi <dankyiboateng15@gmail.com>
2022-11-15 22:44:34 +0000

    deliveroo-clone: minor adjustments to the header

--- deliveroo-clone/screens/HomeScreen.jsx
+++ deliveroo-clone/screens/HomeScreen.jsx
@@ -9,6 +9,8 @@ import {
 	MagnifyingGlassIcon as MagnifyingGlassIcon
 } from "react-native-heroicons/outline";
 
+import Categories from "../components/Categories";
+
 const HomeScreen = () => {
 	const navigation = useNavigation();
 
@@ -19,46 +21,44 @@ const HomeScreen = () => {
 	}, [])
 
 	return (
-		<SafeAreaView className="bg-white">
-			<View className="pt-3">
-				{/* Header */}
-				<View className="flex-row pb-3 items-center mx-4 space-x-2">
-					<View>
-						<Image
-							source={{ uri: "https://links.papareact.com/wru" }}
-							className="h-7 w-7 bg-gray-300 p-4 rounded-full"
-						/>
-					</View>
-
-					<View className="flex-1">
-						<Text className="font-bold text-gray-400 text-xs">
-							Deliver Now
-						</Text>
-
-						<Text className="font-bold text-xs">
-							Current Location
-							<ChevronDownIcon size={20} color="#00ccbb" />
-						</Text>
-					</View>
-
-					<View>
-						<UserIcon size={35} color="#00ccbb" />
-					</View>
+		<SafeAreaView className="bg-white pt-3">
+			{/* Header */}
+			<View className="flex-row pb-3 items-center mx-4 space-x-2">
+				<View>
+					<Image
+						source={{ uri: "https://links.papareact.com/wru" }}
+						className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+					/>
 				</View>
 
-				{/* Search */}
-				<View className="flex-row items-center mx-4 space-x-2 pb-2">
-					<View className="flex-row flex-1 p-3 space-x-2 bg-gray-200">
-						<MagnifyingGlassIcon size={20} color="gray" />
-						<TextInput
-							placeholder="Restaurants and Cuisines"
-							keyboardType="default"
-						/>
-					</View>
+				<View className="flex-1">
+					<Text className="font-bold text-gray-400 text-xs">
+						Deliver Now
+					</Text>
 
-					<View>
-						<AdjustmentsVerticalIcon color="#00ccbb" />
-					</View>
+					<Text className="font-bold text-xs">
+						Current Location
+						<ChevronDownIcon size={20} color="#00ccbb" />
+					</Text>
+				</View>
+
+				<View>
+					<UserIcon size={35} color="#00ccbb" />
+				</View>
+			</View>
+
+			{/* Search */}
+			<View className="flex-row items-center mx-4 space-x-2 pb-2">
+				<View className="flex-row flex-1 p-3 space-x-2 bg-gray-200">
+					<MagnifyingGlassIcon size={20} color="gray" />
+					<TextInput
+						placeholder="Restaurants and Cuisines"
+						keyboardType="default"
+					/>
+				</View>
+
+				<View>
+					<AdjustmentsVerticalIcon color="#00ccbb" />
 				</View>
 			</View>
 		</SafeAreaView>
