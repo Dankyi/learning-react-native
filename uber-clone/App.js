514628d8b7206001f65135857c1f0568a1b1c9cd
Dankyi <dankyiboateng15@gmail.com>
2022-11-27 23:16:59 +0000

    uber-clone: configured react navigation stack for homescreen

--- uber-clone/App.js
+++ uber-clone/App.js
@@ -1,15 +1,29 @@
 import { Provider } from "react-redux";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import HomeScreen from "./screens/HomeScreen";
 import store from "./store";
 
 const App = () => {
+	const Stack = createNativeStackNavigator();
+
 	return (
 		<Provider store={store}>
-			<SafeAreaProvider>
-				<HomeScreen />
-			</SafeAreaProvider>
+			<NavigationContainer>
+				<SafeAreaProvider>
+					<Stack.Navigator>
+						<Stack.Screen
+							name="HomeScreen"
+							component={HomeScreen}
+							options={{
+								headerShown: false
+							}}
+						/>
+					</Stack.Navigator>
+				</SafeAreaProvider>
+			</NavigationContainer>
 		</Provider>
 	);
 };
