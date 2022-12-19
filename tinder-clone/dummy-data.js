82523e5a657a5626b537550f3516ef992eb224ce
Dankyi <dankyiboateng15@gmail.com>
2022-12-19 00:41:32 +0000

    tinder-clone: implemented the body (card swiping) section of the homescreen

---
+++ tinder-clone/dummy-data.js
@@ -0,0 +1,74 @@
+export const dummyData = [
+	{
+		id: 1,
+		firstName: "Bob",
+		lastName: "Arnold",
+		job: "Musician",
+		age: 25,
+		photoURL: "https://cdn.pixabay.com/photo/2017/04/19/10/46/virtual-reality-2241857_960_720.jpg"
+	},
+	{
+		id: 2,
+		firstName: "Mavis",
+		lastName: "Ocansey",
+		job: "Dancer",
+		age: 23,
+		photoURL: "https://cdn.pixabay.com/photo/2020/11/30/08/30/woman-5790338_960_720.png"
+	},
+	{
+		id: 3,
+		firstName: "Steve",
+		lastName: "Anderson",
+		job: "Data Analyst",
+		age: 30,
+		photoURL: "https://cdn.pixabay.com/photo/2016/12/07/21/01/cartoon-1890438_960_720.jpg"
+	},
+	{
+		id: 4,
+		firstName: "Sandy",
+		lastName: "Gomez",
+		job: "Hair Stylist",
+		age: 24,
+		photoURL: "https://cdn.pixabay.com/photo/2021/07/19/04/36/woman-6477171_960_720.jpg"
+	},
+	{
+		id: 5,
+		firstName: "Rose",
+		lastName: "Mount",
+		job: "Fashion Designer",
+		age: 27,
+		photoURL: "https://cdn.pixabay.com/photo/2020/10/27/01/00/woman-5689239_960_720.png"
+	},
+	{
+		id: 6,
+		firstName: "Khalid",
+		lastName: "Jamal",
+		job: "Carpenter",
+		age: 48,
+		photoURL: "https://cdn.pixabay.com/photo/2020/12/05/08/40/elder-5805412_960_720.png"
+	},
+	{
+		id: 7,
+		firstName: "Joy",
+		lastName: "Graham",
+		job: "Vocalist",
+		age: 22,
+		photoURL: "https://cdn.pixabay.com/photo/2022/11/24/13/17/illustrations-7614343_1280.png"
+	},
+	{
+		id: 8,
+		firstName: "Ismael",
+		lastName: "Potter",
+		job: "Programmer",
+		age: 21,
+		photoURL: "https://cdn.pixabay.com/photo/2015/05/01/23/20/man-749290_1280.jpg"
+	},
+	{
+		id: 9,
+		firstName: "Rosalinda",
+		lastName: "Wood",
+		job: "Hairdresser",
+		age: 28,
+		photoURL: "https://cdn.pixabay.com/photo/2020/10/10/17/54/girl-5643812_1280.png"
+	}
+];
--- tinder-clone/screens/HomeScreen.jsx
+++ tinder-clone/screens/HomeScreen.jsx
@@ -1,22 +1,26 @@
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
+import Swiper from "react-native-deck-swiper";
 import {
 	AntDesign, Ionicons,
-	Entyp, MaterialCommunityIcons
+	Feather, MaterialCommunityIcons
 } from "@expo/vector-icons";
 import {
 	SafeAreaView, View, TouchableOpacity, Image, Text
 } from "react-native";
 
 import useAuth from "../hooks/useAuth";
+import { dummyData } from "../dummy-data";
 
 const HomeScreen = () => {
-	const { user, logout } = useAuth();
 	const [profilePressed, setProfilePressed] = React.useState(false);
+	const [numCardSwiped, setNumCardSwiped] = React.useState(0);
+	const { user, logout } = useAuth();
 	const navigation = useNavigation();
+	const swipeRef = React.useRef(null);
 
 	return (
-		<SafeAreaView>
+		<SafeAreaView className="flex-1">
 			{/* Header section of HomeScreen */}
 			<View className="flex-row justify-between items-center px-4">
 				{/* Profile picture and its dropdown */}
@@ -69,6 +73,105 @@ const HomeScreen = () => {
 				</TouchableOpacity>
 			</View>
 
+			{/* Swiper Card Section */}
+			<View className="flex-1 -mt-6">
+				<Swiper
+					ref={swipeRef}
+					containerStyle={{ backgroundColor: "tranparent" }}
+					cards={dummyData}
+					cardIndex={0}
+					stackSize={dummyData.length}
+					overlayLabels={{
+						left: {
+							title: "NOPE!",
+							style: {
+								label: {
+									textAlign: "right",
+									color: "#FF5864"
+								}
+							}
+						},
+						right: {
+							title: "MATCH!",
+							style: {
+								label: {
+									textAlign: "left",
+									color: "#4BB543"
+								}
+							}
+						}
+					}}
+					animateCardOpacity
+					onSwipedLeft={() => {
+						// Pass
+					}}
+					onSwipedRight={() => {
+						// Match
+					}}
+					verticalSwipe={false}
+					renderCard={(card) => (
+						<View
+							key={card.id}
+							className="relative bg-[#FF5864] h-3/4 rounded-xl"
+						>
+							<Image
+								className="absolute top-0 w-full h-full rounded-xl"
+								source={{ uri: card.photoURL }}
+							/>
+
+							<View className="flex-row absolute bottom-0 w-full px-6
+								py-2 bg-white justify-between shadow-sm rounded-b-xl"
+							>
+								<View>
+									<Text className="text-lg font-bold">
+										{card.firstName} {card.lastName}
+									</Text>
+									<Text>{card.job}</Text>
+								</View>
+
+								<Text className="text-lg font-bold">{card.age}</Text>
+							</View>
+						</View>
+					)}
+				/>
+			</View>
+
+			{/* Cancel and Like Section */}
+			<View className="flex-row justify-evenly mb-4">
+				{/* Cancel Button */}
+				<TouchableOpacity
+					className="w-16 h-16 justify-center items-center
+						bg-[#FF5864] rounded-full"
+					onPress={() => {
+						// Only swipe if there are some cards.
+						// Print "swipeRef.current "to see all of its properties
+						const stackSize = swipeRef.current.props.stackSize;
+						if (numCardSwiped < stackSize) {
+							swipeRef.current.swipeLeft();
+							setNumCardSwiped(prevNum => prevNum + 1);
+						}
+					}}
+				>
+					<Feather name="x" size={26} />
+				</TouchableOpacity>
+
+				{/* Like Button */}
+				<TouchableOpacity
+					className="w-16 h-16 justify-center items-center
+						bg-[#4BB543] rounded-full"
+					onPress={() => {
+						// Only swipe if there are some cards.
+						// Print "swipeRef.current "to see all of its properties
+						const stackSize = swipeRef.current.props.stackSize;
+						if (numCardSwiped < stackSize) {
+							swipeRef.current.swipeRight();
+							setNumCardSwiped(prevNum => prevNum + 1);
+						}
+					}}
+				>
+					<AntDesign name="heart" size={24} />
+				</TouchableOpacity>
+			</View>
 		</SafeAreaView>
 	);
 };
