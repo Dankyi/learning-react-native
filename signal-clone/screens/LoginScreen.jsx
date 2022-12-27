161ebea347a9afb09e4fe79731dd20e7bef03b0c
Dankyi <dankyiboateng15@gmail.com>
2022-12-27 18:17:58 +0000

    signal-clone: improved the loginscreen design ensuring keyboard hides onpress of outside environment

--- signal-clone/screens/LoginScreen.jsx
+++ signal-clone/screens/LoginScreen.jsx
@@ -1,5 +1,8 @@
 import React from "react";
-import { View, KeyboardAvoidingView, Platform } from "react-native";
+import {
+	View, KeyboardAvoidingView, Platform,
+	TouchableWithoutFeedback, Keyboard
+} from "react-native";
 import { Button, Input, Image } from "@rneui/themed";
 import { StatusBar } from "expo-status-bar";
 
@@ -8,42 +11,49 @@ const LoginScreen = () => {
 	const [password, setPassword] = React.useState("");
 
 	return (
-		<KeyboardAvoidingView
-			className="flex-1 items-center justify-center p-10"
-			behavior={Platform.OS === "ios" ? "padding" : "height"}
-			keyboardVerticalOffset={10}
-		>
-			<StatusBar style="light" />
+		<TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+			<KeyboardAvoidingView
+				className="flex-1 items-center px-10"
+				behavior={Platform.OS === "ios" ? "padding" : "height"}
+				keyboardVerticalOffset={10}
+			>
+				<StatusBar style="light" />
 
-			<Image
-				style={{ width: 160, height: 160, marginBottom: 20 }}
-				source={require("../assets/signal.png")}
-			/>
+				<View className="mt-10">
+					<Image
+						style={{ width: 160, height: 160, marginBottom: 20 }}
+						source={require("../assets/signal.png")}
+					/>
+				</View>
 
-			<View className="w-72">
-				<Input
-					placeholder="Email"
-					autoFocus
-					type="Email"
-					value={email}
-					onChangeText={(text) => setEmail(text)}
-				/>
+				<View className="w-full my-4 space-y-2">
+					<Input
+						placeholder="Email"
+						autoFocus
+						type="email"
+						value={email}
+						onChangeText={(text) => setEmail(text)}
+					/>
 
-				<Input
-					placeholder="Password"
-					type="password"
-					secureTextEntry
-					value={password}
-					onChangeText={(text) => setPassword(text)}
-				/>
-			</View>
+					<Input
+						placeholder="Password"
+						type="password"
+						secureTextEntry
+						value={password}
+						onChangeText={(text) => setPassword(text)}
+					/>
+				</View>
 
-			<View className="mb-20 space-y-3 w-44">
-				<Button title="LOGIN" />
+				<View className="mb-20 space-y-5 w-44">
+					<Button title="LOGIN" />
 
-				<Button title="REGISTER" type="outline" />
-			</View>
-		</KeyboardAvoidingView>
+					<Button
+						title="REGISTER"
+						type="outline"
+					/>
+				</View>
+			</KeyboardAvoidingView>
+		</TouchableWithoutFeedback>
 	);
 };
 
